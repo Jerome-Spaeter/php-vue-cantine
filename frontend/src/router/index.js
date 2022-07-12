eafb5c5cbc5fb9131abfeadf776534913ddfa317
unknown <boutin.loic@hotmail.com>
2022-07-12 11:01:17 +0200

    first commit

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,11 +1,11 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import AccueilView from '../views/AccueilView.vue'
 
 const routes = [
   {
     path: '/',
-    name: 'home',
-    component: HomeView
+    name: 'accueil',
+    component: AccueilView
   },
   {
     path: '/about',
