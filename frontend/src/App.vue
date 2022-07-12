d17a8f438bfb99dff444850914647dcef34f4346
samir <kafka_67@hotmail.fr>
2022-07-12 16:44:05 +0200

    feature/add connexion

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,30 +1,42 @@
 <template>
   <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <div class="liens">
+      <router-link to="/">Accueil</router-link>
+      <router-link to="/connexion">Connexion</router-link>
+      <router-link to="/inscription">Inscription</router-link>
+    </div>
   </nav>
   <router-view/>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+<style>
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+    font-size: 18px;
   }
+
+nav{
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+.liens{
+  width: 70%;
+  display: flex;
+  justify-content: space-evenly;
+}
+img{
+  width: 8%;
+}
+a{
+  text-decoration: none;
+  font-size: 2rem;
+  font-weight: bold;
+  color: black ;
+}
+a.router-link-exact-active {
+  color: #FFBF38;
 }
 </style>
---
+++ frontend/src/views/ConnexionView.vue
@@ -0,0 +1,66 @@
+<template>
+    <body>
+        <div class="connexion">
+            <section class="image">
+                <div ></div>
+            </section>
+            <section class="login">
+                <h1>Connexion</h1>
+                <form>
+                    <div class="user">
+                        <label for="user_mail">
+                            <input type="text" id="user-identifiant" name="user_identifiant" placeholder="Identifiant">
+                        </label>
+                        <label for="password">
+                            <input type="text" id="user-mdp" name="user_mdp" placeholder="Mot de passe">
+                        </label>
+                        <label for="btn_mdp_oublie">
+                            <input type="button" id="btn_mdp_oublie" value="Mot de passe oublié?">
+                        </label>
+                    </div>
+                </form>
+            </section>
+        </div>
+    </body>
+</template>
+
+<script>
+export default {
+  name: 'connexion',
+};
+</script>
+
+<style>
+
+body{
+    height: 100%;
+}
+
+.connexion {
+    border: 1px solid black;
+    height: 100vh;
+    display: flex;
+    justify-content: space-evenly;
+    margin-top: 3rem;
+}
+
+.image {
+    width: 50%;
+    background-image: url("@/assets/img/image_connexion.jpg");
+    height: 100vh;
+    background-position: center right;
+    background-repeat: no-repeat;
+    background-size: cover;
+    }
+
+.login{
+    border: solid 1px black;
+    width: 50%;
+    display: flex;
+    flex-direction:column;
+}
+
+h1 {
+    text-align: center;
+}
+</style>
