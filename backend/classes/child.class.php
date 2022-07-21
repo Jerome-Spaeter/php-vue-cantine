76705b836f0750329b2360e8babc8953dbb87cf7
Sam S <shbibsamy@gmail.com>
2022-07-21 11:50:53 +0200

    feat/create-child

--- backend/classes/child.class.php
+++ backend/classes/child.class.php
@@ -1,32 +1,72 @@
 <?php
-class Child {
-        public $table; // name of the table
+    class Child {
+        private $dbTable = 'child'; // name of the table
 
-        protected $childId;
-        protected $firstname;
-        protected $surname;
-        protected $dob;
-        protected $allergies;
-        protected $board;
-        protected $inscriptionDate;
-        protected $validated;
-        protected $deleted;
+        public $connect;
+
+        public $childId;
+        public $parentId;
+        public $firstname;
+        public $surname;
+        public $dob;
+        public $allergies;
+        public $board;
+        public $inscriptionDate;
+        public $validated = false;
+        public $deleted = false;
+
+        public function __construct($db) {
+            $this->connect = $db;
+        }
 
         public function createChild(){
-        //    <code to be added>
+            $sqlQuery = "INSERT INTO
+                        ". $this->dbTable ."
+                    SET
+                    fk_parent_id = :parentId,
+                    child_firstname = :firstname,
+                    child_surname = :surname,
+                    child_dob = :dob,
+                    child_allergies = :allergies,
+                    child_board = :board,
+                    child_inscription_date = now(),
+                    child_deleted = :deleted,
+                    child_validated = :validated;";
+
+            $stmt = $this->connect->prepare($sqlQuery);
+            $this->parentId=htmlspecialchars(strip_tags($this->parentId));
+            $this->firstname=htmlspecialchars(strip_tags($this->firstname));
+            $this->surname=htmlspecialchars(strip_tags($this->surname));
+            $this->dob=htmlspecialchars(strip_tags($this->dob));
+            $this->allergies=htmlspecialchars(strip_tags($this->allergies));
+            $this->board=htmlspecialchars(strip_tags($this->board));
+
+            $stmt->bindParam(":parentId", $this->parentId);
+            $stmt->bindParam(":firstname", $this->firstname);
+            $stmt->bindParam(":surname", $this->surname);
+            $stmt->bindParam(":dob", $this->dob);
+            $stmt->bindParam(":allergies", $this->allergies);
+            $stmt->bindParam(":board", $this->board);
+            $stmt->bindParam(":deleted", $this->deleted);
+            $stmt->bindParam(":validated", $this->validated);
+        
+            if($stmt->execute()){
+                return true;
+            }
+            return false;
         }
 
         public function get(){       
-                return $currentChildInformation = [
-                'firstname' => $this-> firstname,
-                'surname' => $this-> surname,
-                'dob' => $this-> dob,
-                'allergies' => $this-> allergies,
-                'board' => $this-> board,
-                'inscriptionDate' => $this-> inscriptionDate,
-                'validated' => $this-> validated,
-                'deleted' => $this-> deleted,
-                ];
+            return $currentChildInformation = [
+            'firstname' => $this-> firstname,
+            'surname' => $this-> surname,
+            'dob' => $this-> dob,
+            'allergies' => $this-> allergies,
+            'board' => $this-> board,
+            'inscriptionDate' => $this-> inscriptionDate,
+            'validated' => $this-> validated,
+            'deleted' => $this-> deleted,
+            ];
         }
 
         public function delete(){
@@ -39,5 +79,5 @@ class Child {
         public function validate(){
 
         }
-        }
+    }
 ?>
---
+++ backend/scripts/script_child_create.php
@@ -0,0 +1,30 @@
+<?php
+    require './classes/database_handler.class.php';
+    require './classes/child.class.php';
+
+    $database = new DatabaseHandler();
+    $db = $database -> connectToDatabase();
+
+    $item = new Child ($db);
+
+    // Simulate creation of a new child:
+    // $simulatedRequest = new stdClass();
+
+    // $simulatedRequest -> parentId = '3';
+    // $simulatedRequest -> firstname = 'samy';
+    // $simulatedRequest -> surname = 'shbib';
+    // $simulatedRequest -> dob = '1989-06-08';
+    // $simulatedRequest -> allergies = '';
+    // $simulatedRequest -> board = '1';
+
+    $data = json_decode(file_get_contents("php://input"));
+    // $data = $simulatedRequest;
+
+    $item->parentId = $data->parentId;
+    $item->firstname = $data->firstname;
+    $item->surname = $data->surname;
+    $item->dob = $data->dob;
+    $item->allergies = $data->allergies;
+    $item->board = $data->board;
+    $item->createChild();    
+?>
