575bc5f4af1c3c33e0e361cc9903d3fee56b470a
unknown <boutin.loic@hotmail.com>
2022-07-22 11:20:17 +0200

    feature/fin css form

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -4,24 +4,24 @@
       <h1>Inscrire un gestionnaire de service</h1>
       <form>
         <label>
-          <input type="text" placeholder="Nom">
+          <input type="text" name="user_firstname" placeholder="Nom">
         </label>
         <label>
-          <input type="text" placeholder="Prénom">
+          <input type="text" name="user_surname" placeholder="Prénom">
         </label>
         <label>
-          <input type="text" placeholder="Email">
+          <input type="text" name="user_email" placeholder="Email">
         </label>
         <label>
-          <input type="password" placeholder="Mot de passe">
+          <input type="password" name="user_password" placeholder="Mot de passe">
         </label>
         <label>
-          <input type="password" placeholder="Comfirmation du mot de passe">
+          <input type="password" name="re_user_password" placeholder="Comfirmation du mot de passe">
         </label>
-        <button type="submit" value="Valider"></button>
+        <button type="submit">Valider</button>
       </form>
     </section>
-    <section class="liste-gestionnaire">
+    <section class="liste-gestionnaires">
       <h1>Liste des gestionnaires de service</h1>
     </section>
   </div>
@@ -38,3 +38,46 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+h1{
+  text-align: start;
+  font-size: 1.7rem;
+} 
+.accueil-super-admin{
+  width: 70%;
+  margin: auto;
+}
+form{
+  width: 90%;
+  margin: auto;
+  box-shadow: 10px 5px 5px 5px #F0F0F0;
+  border-radius: 30px;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-around;
+}
+input{
+  border-radius: 30px;
+  background-color: #F0F0F0;
+  width: 20rem;
+  padding: 0.8rem;
+  margin: 1rem;
+  text-align: center;
+  font-size: 1rem;
+  border: none;
+}
+button{
+  width: 20%;
+  border-radius: 30px;
+  margin: 1rem; 
+  border:none;
+  color: white;
+  background-color: #FFBF38;
+  font-size: 1.4rem;
+  font-weight: 700;
+}
+button:hover{
+  background-color: #FFDC92
+}
+</style>
